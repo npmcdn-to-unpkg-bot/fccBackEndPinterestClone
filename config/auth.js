6a4ae847972292fd99084ef9b8f6bba0d202e53a
sirsandmann <florian@sander-net.de>
2016-04-20 22:39:11 +0200

    modified callback

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'       : process.env.TWTRCC,
         'consumerSecret'    : process.env.TWTRCS,
-        'callbackURL'       : "" + process.env.IP + process.env.PORT + '/auth/twitter/callback'
+        'callbackURL'       : process.env.IP + '/auth/twitter/callback'
     }
 };
