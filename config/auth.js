6a584c1d983e5e5802cc22862fbdaf0e0f935603
sirsandmann <florian@sander-net.de>
2016-04-20 23:10:11 +0200

    Runnable Version

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'       : process.env.TWTRCC,
         'consumerSecret'    : process.env.TWTRCS,
-        'callbackURL'       : process.env.IP + '/auth/twitter/callback'
+        'callbackURL'       : '/auth/twitter/callback'
     }
 };
